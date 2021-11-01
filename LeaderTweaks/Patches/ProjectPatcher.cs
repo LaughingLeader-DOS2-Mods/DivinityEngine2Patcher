c0afbfce9cb944ecce0030dfa0e15b6ab4567e4f
LaughingLeader <laughingleader@gmail.com>
2021-11-01 06:19:22 -0500

    Testing getting tags to work better

--- LeaderTweaks/Patches/ProjectPatcher.cs
+++ LeaderTweaks/Patches/ProjectPatcher.cs
@@ -12,14 +12,13 @@ using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
+using System.Windows.Controls.Primitives;
 
 namespace LeaderTweaks.Patches
 {
-	[LeaderPatcher]
+	[LeaderPatcher("Project Settings")]
 	public class ProjectPatcher : IPatcher
 	{
-		private static bool loaded = false;
-
 		private static Dictionary<string, PropertyInfo> MetaStringAttributes;
 
 		static readonly MethodInfo add_Click = AccessTools.Method(typeof(System.Windows.Forms.ToolStripItem), "add_Click");
@@ -27,19 +26,23 @@ namespace LeaderTweaks.Patches
 		static readonly MethodInfo m_ProjectAddClickEvent = AccessTools.Method(typeof(ProjectPatcher), nameof(ProjectPatcher.ProjectPlugin_AddClickEvent));
 		static readonly MethodInfo m_PublishAddClickEvent = AccessTools.Method(typeof(ProjectPatcher), nameof(ProjectPatcher.PublishPlugin_AddClickEvent));
 		static readonly MethodInfo m_ProjectOpenSettings = AccessTools.Method(typeof(ProjectPlugin), "OpenSettings");
-		static readonly MethodInfo m_PublishCreatePanel = AccessTools.Method(typeof(ProjectPlugin), nameof(PublishPlugin.CreatePanel));
+		static readonly MethodInfo m_PublishCreatePanel = AccessTools.Method(typeof(PublishPlugin), nameof(PublishPlugin.CreatePanel));
 
 		public void Init(Harmony harmony)
 		{
-			if (loaded) return;
-
 			var pt = typeof(ProjectPatcher);
 
 			harmony.Patch(AccessTools.Method(typeof(ProjectPlugin), nameof(ProjectPlugin.Start)),
 				transpiler: new HarmonyMethod(AccessTools.Method(pt, nameof(ProjectPatcher.t_ProjectPlugin_Start))));
+
 			harmony.Patch(AccessTools.Method(typeof(PublishPlugin), nameof(PublishPlugin.RegisterPanel)),
 				transpiler: new HarmonyMethod(AccessTools.Method(pt, nameof(ProjectPatcher.t_PublishPlugin_RegisterPanel))));
-			loaded = true;
+
+			harmony.Patch(AccessTools.Method(typeof(PublishWindow), nameof(PublishWindow.GetSelectedWorkshopFeatureTags)),
+				postfix: new HarmonyMethod(AccessTools.Method(pt, nameof(ProjectPatcher.GetSelectedWorkshopFeatureTags))));
+
+			harmony.Patch(AccessTools.Method(typeof(PublishWindow), nameof(PublishWindow.SetSelectedWorkshopFeatureTags)),
+				postfix: new HarmonyMethod(AccessTools.Method(pt, nameof(ProjectPatcher.SetSelectedWorkshopFeatureTags))));
 
 			var t2 = typeof(ModSettings);
 
@@ -92,7 +95,10 @@ namespace LeaderTweaks.Patches
 			LSToolFramework.ToolFramework.Instance?.MessageService?.PostInfoMessage("[LeaderTweaks] Added 'Click' event listener to project settings button.");
 			toolStripItem.Click += (s, e) =>
 			{
-				RefreshMeta(s, e);
+				if (LSToolFramework.ToolFramework.Instance != null && !String.IsNullOrWhiteSpace(LSToolFramework.ToolFramework.Instance.ModFolder))
+				{
+					RefreshMeta(s, e);
+				}
 				m_ProjectOpenSettings.Invoke(plugin, new object[] { s, e });
 			};
 		}
@@ -122,11 +128,14 @@ namespace LeaderTweaks.Patches
 
 		public static void PublishPlugin_AddClickEvent(System.Windows.Forms.ToolStripItem toolStripItem, PublishPlugin plugin)
 		{
-			Helper.Log("Added 'Click' event listener to project settings button.", false);
+			Helper.Log("Added 'Click' event listener to publish button.", false);
 			LSToolFramework.ToolFramework.Instance?.MessageService?.PostInfoMessage("[LeaderTweaks] Added 'Click' event listener to project settings button.");
 			toolStripItem.Click += (s,e) =>
 			{
-				RefreshMeta(s, e);
+				if(LSToolFramework.ToolFramework.Instance != null && !String.IsNullOrWhiteSpace(LSToolFramework.ToolFramework.Instance.ModFolder))
+				{
+					RefreshMeta(s, e);
+				}
 				m_PublishCreatePanel.Invoke(plugin, new object[] { s, e });
 			};
 		}
@@ -136,12 +145,11 @@ namespace LeaderTweaks.Patches
 		{
 			try
 			{
-				LSToolFramework.ToolFramework.Instance?.MessageService?.PostInfoMessage("[LeaderTweaks] Reloading meta.lsx");
 				//EoCPlugin.ModBackend.Instance?.InitializeFromCurrentModServer();
 				//FileLog.Log($"[LeaderTweaks] ActiveSettings[{EoCPlugin.ModBackend.Instance?.ActiveSettings}] VersionBuild[{EoCPlugin.ModBackend.Instance?.ActiveSettings?.VersionBuild}]");
 				var settings = EoCPlugin.ModBackend.Instance?.ActiveSettings;
 				string metaFilePath = LSToolFramework.ToolFramework.Instance.GameDataPath.Replace("/", "\\") + "Mods\\" + LSToolFramework.ToolFramework.Instance.ModFolder + "\\meta.lsx";
-				Helper.Log($"Reloading meta.lsx at ({metaFilePath})");
+				Helper.Log($"Reloading meta.lsx at ({metaFilePath})", false);
 				if (File.Exists(metaFilePath) && settings != null)
 				{
 					var root = XElement.Load(metaFilePath);
@@ -149,16 +157,16 @@ namespace LeaderTweaks.Patches
 					{
 						foreach (KeyValuePair<string, PropertyInfo> att in MetaStringAttributes)
 						{
-							var attValue = root.XPathSelectElement($"//node[@id='ModuleInfo']//attribute[@id='{att.Key}']")?.Attribute("value");
+							var attValue = root.XPathSelectElement($"//node[@id='ModuleInfo']//attribute[@id='{att.Key}']")?.Attribute("value")?.Value;
 							if (attValue != null)
 							{
 								att.Value.SetValue(settings, attValue, null);
 							}
 						}
-						var version = root.XPathSelectElement("//node[@id='ModuleInfo']//attribute[@id='Version']")?.Attribute("value");
-						var numPlayers = root.XPathSelectElement("//node[@id='ModuleInfo']//attribute[@id='NumPlayers']")?.Attribute("value");
+						var version = root.XPathSelectElement("//node[@id='ModuleInfo']//attribute[@id='Version']")?.Attribute("value")?.Value;
+						var numPlayers = root.XPathSelectElement("//node[@id='ModuleInfo']//attribute[@id='NumPlayers']")?.Attribute("value")?.Value;
 
-						if (version != null && int.TryParse(version.Value, out int vint))
+						if (version != null && int.TryParse(version, out int vint))
 						{
 							settings.VersionMajor = (vint >> 28);
 							settings.VersionMinor = (vint >> 24) & 0x0F;
@@ -166,27 +174,116 @@ namespace LeaderTweaks.Patches
 							settings.VersionBuild = (vint & 0xFFFF);
 						}
 
-						if (numPlayers != null && int.TryParse(numPlayers.Value, out int num))
+						if (numPlayers != null && int.TryParse(numPlayers, out int num))
 						{
 							settings.NumPlayers = num;
 						}
 
-						Helper.Log("Updated project settings from meta.lsx");
+						Helper.Log("Updated project settings from meta.lsx", false);
 					}
 					else
 					{
-						Helper.Log("Failed to parse meta.lsx.");
+						Helper.Log("Failed to parse meta.lsx.", false);
 					}
 				}
 				else
 				{
-					Helper.Log($"No file found at '${metaFilePath}'?");
+					Helper.Log($"No file found at '${metaFilePath}'?", false);
 				}
 			}
 			catch(Exception ex)
 			{
-				Helper.Log($"Error parsing meta.lsx:\n{ex}");
+				Helper.Log($"Error parsing meta.lsx: {ex}");
 			}
+		}
+
+		public static void GetSelectedWorkshopFeatureTags(ref List<EWorkshopFeatureTags> __result, PublishWindow __instance)
+		{
+			var settings = EoCPlugin.ModBackend.Instance?.ActiveSettings;
+			if(settings != null)
+			{
+				var t = typeof(EWorkshopFeatureTags);
+				var tags = settings.Tags.Split(';').Select(x => x.ToLower()).ToList();
+				foreach (string workshopTag in Enum.GetNames(t))
+				{
+					if (tags.Contains(workshopTag.ToLower()))
+					{
+						EWorkshopFeatureTags enumValue = (EWorkshopFeatureTags)Enum.Parse(t, workshopTag);
+						if(!__result.Contains(enumValue))
+						{
+							__result.Add(enumValue);
+						}
+					}
+				}
+			}
+
+			var t2 = typeof(EPublishTag);
+
+			var projectTagsFixed = __instance.ProjectTags.Distinct().OrderBy(x => Enum.GetName(t2, x));
+			__instance.ProjectTags.Clear();
+			foreach(var tag in projectTagsFixed)
+			{
+				__instance.ProjectTags.Add(tag);
+			}
+
+			Helper.Log($"GetSelectedWorkshopFeatureTags Result: {String.Join(",", __result)}");
+			Helper.Log($"GetSelectedWorkshopFeatureTags ProjectTags: {String.Join(",", __instance.ProjectTags)}");
+		}
+
+		public static void SetSelectedWorkshopFeatureTags(HashSet<EWorkshopFeatureTags> tags, PublishWindow __instance,
+			ToggleButton ___m_ArmorsTag, ToggleButton ___m_BalancingStatsTag, ToggleButton ___m_ClassesTag, 
+			ToggleButton ___m_CompanionsTag, ToggleButton ___m_ConsumablesTag, ToggleButton ___m_MapsTag, 
+			ToggleButton ___m_OriginsTag, ToggleButton ___m_OverhaulsTag, ToggleButton ___m_QualityOfLifeTag, 
+			ToggleButton ___m_QuestsTag, ToggleButton ___m_RacesTag, ToggleButton ___m_RunesBoostsTag, ToggleButton ___m_SkillsTag, 
+			ToggleButton ___m_UtilityTag, ToggleButton ___m_VisualOverridesTag, ToggleButton ___m_WeaponsTag)
+		{
+			List<EWorkshopFeatureTags> foundTags = new List<EWorkshopFeatureTags>();
+			var settings = EoCPlugin.ModBackend.Instance?.ActiveSettings;
+			if(settings != null)
+			{
+				var t = typeof(EWorkshopFeatureTags);
+				var stringTags = settings.Tags.Split(';').Select(x => x.ToLower()).ToList();
+				foreach (string workshopTag in Enum.GetNames(t))
+				{
+					if (stringTags.Contains(workshopTag.ToLower()))
+					{
+						EWorkshopFeatureTags enumValue = (EWorkshopFeatureTags)Enum.Parse(t, workshopTag);
+						if(!foundTags.Contains(enumValue))
+						{
+							foundTags.Add(enumValue);
+						}
+					}
+				}
+			}
+
+			___m_ArmorsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Armors) : false;
+			___m_BalancingStatsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.BalancingStats) : false;
+			___m_ClassesTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Classes) : false;
+			___m_CompanionsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Companions) : false;
+			___m_ConsumablesTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Consumables) : false;
+			___m_MapsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Maps) : false;
+			___m_OriginsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Origins) : false;
+			___m_OverhaulsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Overhauls) : false;
+			___m_QualityOfLifeTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.QualityOfLife) : false;
+			___m_QuestsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Quests) : false;
+			___m_RacesTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Races) : false;
+			___m_RunesBoostsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.RunesBoosts) : false;
+			___m_SkillsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Skills) : false;
+			___m_UtilityTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Utility) : false;
+			___m_VisualOverridesTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.VisualOverrides) : false;
+			___m_WeaponsTag.IsChecked = ___m_ArmorsTag.IsChecked != true ? foundTags.Contains(EWorkshopFeatureTags.Weapons) : false;
+
+			var t2 = typeof(EPublishTag);
+
+			var projectTagsFixed = __instance.ProjectTags.Distinct().OrderBy(x => Enum.GetName(t2, x));
+			__instance.ProjectTags.Clear();
+			foreach(var tag in projectTagsFixed)
+			{
+				__instance.ProjectTags.Add(tag);
+			}
+
+			Helper.Log($"SetSelectedWorkshopFeatureTags Result: {String.Join(",", foundTags)}");
+			Helper.Log($"SetSelectedWorkshopFeatureTags ProjectTags: {String.Join(",", __instance.ProjectTags)}");
 		}
 	}
 }
