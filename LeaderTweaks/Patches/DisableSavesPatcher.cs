6a3f97ff617462f09f45733e9596200ad4888bda
LaughingLeader <laughingleader@gmail.com>
2022-12-10 02:33:47 -0600

    Add an experimental patch to try and disable save loading
    The editor appears to try and load module data from all of the saves in the current profile, so this may speed up loading.

---
+++ LeaderTweaks/Patches/DisableSavesPatcher.cs
@@ -0,0 +1,35 @@
+﻿using EoCPlugin;
+
+using HarmonyLib;
+
+using MessagePlugin;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace LeaderTweaks.Patches
+{
+	[LeaderPatcher("Disable Saves Patch")]
+	public class DisableSavesPatcher : IPatcher
+	{
+		public void Init(Harmony patcher)
+		{
+			var pt = typeof(DisableSavesPatcher);
+			var t = typeof(BrowserPluginHelper);
+
+			patcher.Patch(AccessTools.Method(t, nameof(BrowserPluginHelper.SetupSaveGames)),
+				transpiler: new HarmonyMethod(AccessTools.Method(pt, nameof(DisableSavesPatcher.t_DisableSaveModuleLoading))));
+		}
+
+		/* 
+		 * Disables this function which *may* resuls in the editor trying to load all modules from your saves.
+		*/
+		public static IEnumerable<CodeInstruction> t_DisableSaveModuleLoading(IEnumerable<CodeInstruction> instr)
+		{
+			yield return new CodeInstruction(OpCodes.Ret);
+		}
+	}
+}
